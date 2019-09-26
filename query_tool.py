cb335c2acedd56a474b6937cf298538a7ced87de
Tristan <40725906+tgregory98@users.noreply.github.com>
2019-09-26 21:43:57 +0100

    Added master class SchemaBuilder

--- query_tool.py
+++ query_tool.py
@@ -8,12 +8,74 @@ from py2neo import Database, Graph, Transaction, Cursor
 db = Database("bolt://localhost:7687", auth=("neo4j", "cayley"))
 g = Graph("bolt://localhost:7687", auth=("neo4j", "cayley"))
 
-class PairwiseSchemaBuilder:
 
+def commit_cypher_code(cypher):
+    tr = Transaction(g)
+    tr.run(cypher)
+    tr.commit()
+
+
+class SchemaBuilder:
+    def cypher_url_gen(self, sparql_query):
+        wrapper = SPARQLWrapper("http://dbpedia.org/sparql")
+        wrapper.setQuery(sparql_query)
+        wrapper.setReturnFormat("csv")
+        query_result = wrapper.query()
+        url = query_result.geturl()
+
+        return url
+
+    def build_cypher_query(self, depth):
+        sparql_query = self.sparql_query_gen(depth)
+        url = self.cypher_url_gen(sparql_query)
+        cypher_initial = self.cypher_query_gen(depth, url)
+
+        return cypher_initial
+
+    def build_at_depth(self, depth):
+        print("Initiating depth " + str(depth) + " build at " + str(datetime.datetime.now().time()) + ".")
+
+        cypher_build_node_ids = """
+MATCH (x)
+
+SET (CASE left(x.iri,28) WHEN "http://dbpedia.org/resource/" THEN x END).id = toString(replace(x.iri,"http://dbpedia.org/resource/","")), (CASE left(x.iri,28) WHEN "http://dbpedia.org/resource/" THEN x END).prefix = "http://dbpedia.org/resource/"
+
+SET (CASE left(x.iri,37) WHEN "http://dbpedia.org/resource/Category:" THEN x END).id = toString(replace(x.iri,"http://dbpedia.org/resource/Category:","")), (CASE left(x.iri,37) WHEN "http://dbpedia.org/resource/Category:" THEN x END).prefix = "http://dbpedia.org/resource/Category:"
+"""
+
+        cypher_build_edge_ids = """
+MATCH (x)-[y]->(z)
+
+SET (CASE left(y.iri,25) WHEN "http://purl.org/dc/terms/" THEN y END).id = toString(replace(y.iri,"http://purl.org/dc/terms/","")), (CASE left(y.iri,25) WHEN "http://purl.org/dc/terms/" THEN y END).prefix = "http://purl.org/dc/terms/"
+
+SET (CASE left(y.iri,36) WHEN "http://www.w3.org/2004/02/skos/core#" THEN y END).id = toString(replace(y.iri,"http://www.w3.org/2004/02/skos/core#","")), (CASE left(y.iri,36) WHEN "http://www.w3.org/2004/02/skos/core#" THEN y END).prefix = "http://www.w3.org/2004/02/skos/core#"
+"""
+
+        cypher_build_article_labels = """
+MATCH (x {prefix: "http://dbpedia.org/resource/"})
+SET x:article
+"""
+
+        cypher_build_category_labels = """
+MATCH (x {prefix: "http://dbpedia.org/resource/Category:"})
+SET x:category
+"""
+        
+        cypher_query = self.build_cypher_query(depth)
+        cypher_set = [cypher_query, cypher_build_node_ids, cypher_build_edge_ids, cypher_build_article_labels, cypher_build_category_labels]
+        tr = Transaction(g)
+        for cypher in cypher_set:
+            tr.run(cypher)
+        tr.commit()
+
+        print("Depth " + str(depth) + " build complete at " + str(datetime.datetime.now().time()) + ".")
+
+
+class PairwiseSchemaBuilder(SchemaBuilder):
     def __init__(self, start_page, end_page, filter_set):
         self.start_page = start_page
         self.end_page = end_page
-        self.filter_set = filter_set if filter_set is not None else []
+        self.filter_set = filter_set
 
     def sparql_query_gen(self, depth):
         query_part1 = "\rSELECT "
@@ -79,20 +141,8 @@ class PairwiseSchemaBuilder:
         """
         query_part2 = query_part2 + final_block.replace("&&", str(depth)).replace("&", str(depth-1))
 
-        # Build the "FILTER()" expression first.
-        # Then write a .replace("&&&", ...) method using "&&&" as a placeholder for the "FILTER()" expression.
-
         query = query_part1 + query_part2
         return query
-
-    def cypher_url_gen(self, sparql_query):
-        wrapper = SPARQLWrapper("http://dbpedia.org/sparql")
-        wrapper.setQuery(sparql_query)
-        wrapper.setReturnFormat("csv")
-        query_result = wrapper.query()
-        url = query_result.geturl()
-
-        return url
 
     def cypher_query_gen(self, depth, url):
         query_part1 = "WITH \"" + url + "\" AS url\r\rLOAD CSV WITH HEADERS FROM url AS row\r\r"
@@ -123,62 +173,101 @@ class PairwiseSchemaBuilder:
         query = query_part1 + query_part2 + query_part3 + query_part4
         return query
 
-    def cypher_query_gen_from_depth(self, depth):
-        sparql_query = self.sparql_query_gen(depth)
-        url = self.cypher_url_gen(sparql_query)
-        cypher_initial = self.cypher_query_gen(depth, url)
 
-        return cypher_initial
+class ParentSchemaBuilder(SchemaBuilder):
+    def __init__(self, page, filter_set):
+        self.page = page
+        self.filter_set = filter_set
 
-    def build_at_depth(self, depth):
-        cypher_build_node_ids = """
-MATCH (x)
-
-SET (CASE left(x.iri,28) WHEN "http://dbpedia.org/resource/" THEN x END).id = toString(replace(x.iri,"http://dbpedia.org/resource/","")), (CASE left(x.iri,28) WHEN "http://dbpedia.org/resource/" THEN x END).prefix = "http://dbpedia.org/resource/"
-
-SET (CASE left(x.iri,37) WHEN "http://dbpedia.org/resource/Category:" THEN x END).id = toString(replace(x.iri,"http://dbpedia.org/resource/Category:","")), (CASE left(x.iri,37) WHEN "http://dbpedia.org/resource/Category:" THEN x END).prefix = "http://dbpedia.org/resource/Category:"
-"""
-
-        cypher_build_edge_ids = """
-MATCH (x)-[y]->(z)
-
-SET (CASE left(y.iri,25) WHEN "http://purl.org/dc/terms/" THEN y END).id = toString(replace(y.iri,"http://purl.org/dc/terms/","")), (CASE left(y.iri,25) WHEN "http://purl.org/dc/terms/" THEN y END).prefix = "http://purl.org/dc/terms/"
-
-SET (CASE left(y.iri,36) WHEN "http://www.w3.org/2004/02/skos/core#" THEN y END).id = toString(replace(y.iri,"http://www.w3.org/2004/02/skos/core#","")), (CASE left(y.iri,36) WHEN "http://www.w3.org/2004/02/skos/core#" THEN y END).prefix = "http://www.w3.org/2004/02/skos/core#"
-"""
-
-        cypher_build_article_labels = """
-MATCH (x {prefix: "http://dbpedia.org/resource/"})
-SET x:article
-"""
-
-        cypher_build_category_labels = """
-MATCH (x {prefix: "http://dbpedia.org/resource/Category:"})
-SET x:category
-"""
+    def sparql_query_gen(self, depth):
+        query_part1 = "\rSELECT "
+        for i in range(depth):
+            i = i + 1
+            string = "?pred& ?n& "
+            query_part1 = query_part1 + string.replace("&", str(i))
         
-        cypher_query = self.cypher_query_gen_from_depth(depth)
-        cypher_set = [cypher_query, cypher_build_node_ids, cypher_build_edge_ids, cypher_build_article_labels, cypher_build_category_labels]
-        tr = Transaction(g)
-        for cypher in cypher_set:
-            tr.run(cypher)
-        tr.commit()
-        print("Depth " + str(depth) + " build complete at " + str(datetime.datetime.now().time()) + ".")
+        for i in range(len(self.filter_set)):
+            if i == 0:
+                filter_query_pred = "FILTER("
+                string = "regex(?pred&, &&)"
+                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set[i-1]))
+            elif i <= len(self.filter_set) - 1:
+                string = "||regex(?pred&, &&)"
+                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set[i-1]))
+        filter_query_pred = filter_query_pred + ")"
+
+        query_part2 = """
+    WHERE {
+        {
+            """ + filter_query_pred.replace("&", "1") + """
+            <""" + self.page + """> ?pred1 ?n1
+        } .
+        """
+        for i in range(depth-1):
+            i = i + 1
+            block = """
+        {
+            """ + filter_query_pred.replace("&", str(i+1)) + """
+            ?n& ?pred&& ?n&&
+        } .
+            """
+            query_part2 = query_part2 + block.replace("&&", str(i+1)).replace("&", str(i))
+
+        final_string = """
+    }
+        """
+        query_part2 = query_part2 + final_string
+
+        query = query_part1 + query_part2
+        return query
+
+    def cypher_query_gen(self, depth, url):
+        query_part1 = "WITH \"" + url + "\" AS url\r\rLOAD CSV WITH HEADERS FROM url AS row\r\r"
+
+        query_part2 = "MERGE (n0:page {iri: \"" + self.page + "\"})\r"
+        for i in range(depth):
+            i = i + 1
+            string = "MERGE (n&:page {iri: row.n&})\r"
+            query_part2 = query_part2 + string.replace("&", str(i))
+
+        query_part3 = ""
+        for i in range(depth):
+            block = """
+    FOREACH (x IN CASE WHEN row.pred&& IS NULL THEN [] ELSE [1] END | MERGE (n&)-[p:pred {iri: row.pred&&}]->(n&&))
+            """
+            query_part3 = query_part3 + block.replace("&&", str(i+1)).replace("&", str(i))
+
+        query_part4 = "\rRETURN "
+        for i in range(depth):
+            string = "n&, "
+            query_part4 = query_part4 + string.replace("&", str(i))
+        final_string = "n&"
+        query_part4 = query_part4 + final_string.replace("&", str(depth))
+
+        query = query_part1 + query_part2 + query_part3 + query_part4
+        return query
 
 
+commit_cypher_code("MATCH (x) DETACH DELETE (x)")
 
+# sch1 = PairwiseSchemaBuilder("http://dbpedia.org/resource/Television", "http://dbpedia.org/resource/Netflix", ["dct:subject", "skos:broader"])
+# sch1.build_at_depth(2)
+# sch1.build_at_depth(3)
+# sch1.build_at_depth(4)
 
-sch = PairwiseSchemaBuilder("http://dbpedia.org/resource/Television", "http://dbpedia.org/resource/Netflix", ["dct:subject", "skos:broader"])
-sch.build_at_depth(2)
-sch.build_at_depth(3)
-sch.build_at_depth(4)
+sch2a = ParentSchemaBuilder("http://dbpedia.org/resource/Netflix", ["dct:subject", "skos:broader", "rdf:type"])
+sch2a.build_at_depth(2)
+
+# sch2b = ParentSchemaBuilder("http://dbpedia.org/resource/Television", ["dct:subject", "skos:broader", "rdf:type"])
+# sch2b.build_at_depth(2)
 
 # TODO LIST
 # + Clean up the code into one file.
-# + Build a class for building schema.
+# + Build a class for building pairwise schema.
 # + Run a sequence of depth increasing queries.
 # + Add a filter option to the class.
-# - Test out different search filters and optimise.
-# - Let the class populate immediate neighbours too.
-# - Optimise depth selection.
+# + Test out different search filters and optimise.
+# + Write a class for building parent schema.
+# + Write an inheritance class for PairwiseSchemaBuilder and ParentSchemaBuilder.
 # - Instead of using higher depths, try looking at immediate neighbours with many edges, and use them as the new start/end nodes from which to build new schema.
+# - Add options for node/ edge specific filters.
