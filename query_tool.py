ec48075b08c44b3f6a660320aa66024334a922de
Tristan <40725906+tgregory98@users.noreply.github.com>
2019-09-28 21:08:37 +0100

    Code cleanup, options for node/edge specific filters, adding some of this code to SchemaBuilder, starting PopulateSchemaBuilder subclass

--- query_tool.py
+++ query_tool.py
@@ -16,8 +16,70 @@ def commit_cypher_code(cypher):
 
 
 class SchemaBuilder:
-    def __init__(self):
+    def __init__(self, filter_set_edges=[], filter_set_vertices=[]):
         self.name = "SchemaBuilder"
+        self.filter_set_edges = filter_set_edges
+        self.filter_set_vertices = filter_set_vertices
+
+    def filter_query_pred_gen(self):
+        filter_query_pred = ""
+        for i in range(len(self.filter_set_edges)):
+            if len(self.filter_set_edges) == 0:
+                break
+            elif len(self.filter_set_edges) == 1:
+                string = "FILTER(regex(?pred&, &&))"
+                filter_query_pred = string.replace("&&", str(self.filter_set_edges[0]))
+            elif i == 0:
+                filter_query_pred = "FILTER("
+                string = "regex(?pred&, &&)"
+                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set_edges[i]))
+            elif i < len(self.filter_set_edges) - 1:
+                string = "||regex(?pred&, &&)"
+                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set_edges[i]))
+            elif i == len(self.filter_set_edges) - 1:
+                string = "||regex(?pred&, &&))"
+                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set_edges[i]))
+        return filter_query_pred
+
+    def filter_query_pred_inv_gen(self):
+        filter_query_pred_inv = ""
+        for i in range(len(self.filter_set_edges)):
+            if len(self.filter_set_edges) == 0:
+                break
+            elif len(self.filter_set_edges) == 1:
+                string = "FILTER(regex(?pred_inv&, &&))"
+                filter_query_pred_inv = string.replace("&&", str(self.filter_set_edges[0]))
+            elif i == 0:
+                filter_query_pred_inv = "FILTER("
+                string = "regex(?pred_inv&, &&)"
+                filter_query_pred_inv = filter_query_pred_inv + string.replace("&&", str(self.filter_set_edges[i]))
+            elif i < len(self.filter_set_edges) - 1:
+                string = "||regex(?pred_inv&, &&)"
+                filter_query_pred_inv = filter_query_pred_inv + string.replace("&&", str(self.filter_set_edges[i]))
+            elif i == len(self.filter_set_edges) - 1:
+                string = "||regex(?pred_inv&, &&))"
+                filter_query_pred_inv = filter_query_pred_inv + string.replace("&&", str(self.filter_set_edges[i]))
+        return filter_query_pred_inv
+
+    def filter_query_vertex_gen(self):
+        filter_query_vertex = ""
+        for i in range(len(self.filter_set_vertices)):
+            if len(self.filter_set_vertices) == 0:
+                break
+            elif len(self.filter_set_vertices) == 1:
+                string = "FILTER(regex(?n&, &&))"
+                filter_query_vertex = string.replace("&&", str(self.filter_set_vertices[0]))
+            elif i == 0:
+                filter_query_vertex = "FILTER("
+                string = "regex(?n&, &&)"
+                filter_query_vertex = filter_query_vertex + string.replace("&&", str(self.filter_set_vertices[i]))
+            elif i < len(self.filter_set_vertices) - 1:
+                string = "||regex(?n&, &&)"
+                filter_query_vertex = filter_query_vertex + string.replace("&&", str(self.filter_set_vertices[i]))
+            elif i == len(self.filter_set_vertices) - 1:
+                string = "||regex(?n&, &&))"
+                filter_query_vertex = filter_query_vertex + string.replace("&&", str(self.filter_set_vertices[i]))
+        return filter_query_vertex
 
     def cypher_url_gen(self, sparql_query):
         wrapper = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -75,43 +137,39 @@ SET x:category
 
 
 class PairwiseSchemaBuilder(SchemaBuilder):
-    def __init__(self, start_page, end_page, filter_set):
+    def __init__(self, start_page, end_page, filter_set_edges=[], filter_set_vertices=[]):
         self.name = "PairwiseSchemaBuilder between " + start_page + " and " + end_page
         self.start_page = start_page
         self.end_page = end_page
-        self.filter_set = filter_set
+        self.filter_set_edges = filter_set_edges
+        self.filter_set_vertices = filter_set_vertices
 
     def sparql_query_gen(self, depth):
         query_part1 = "\rSELECT "
-        for i in range(depth-1):
-            i = i + 1
+        for i in range(depth - 1):
             string = "?pred& ?pred_inv& ?n& "
-            query_part1 = query_part1 + string.replace("&", str(i))
+            query_part1 = query_part1 + string.replace("&", str(i + 1))
         final_string = "?pred& ?pred_inv&\r"
         query_part1 = query_part1 + final_string.replace("&", str(depth))
-        
-        for i in range(len(self.filter_set)):
-            if i == 0:
-                filter_query_pred = "FILTER("
-                string = "regex(?pred&, &&)"
-                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set[i-1]))
-            elif i <= len(self.filter_set) - 1:
-                string = "||regex(?pred&, &&)"
-                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set[i-1]))
-        filter_query_pred = filter_query_pred + ")"
 
-        for i in range(len(self.filter_set)):
-            if i == 0:
-                filter_query_pred_inv = "FILTER("
-                string = "regex(?pred_inv&, &&)"
-                filter_query_pred_inv = filter_query_pred_inv + string.replace("&&", str(self.filter_set[i-1]))
-            elif i <= len(self.filter_set) - 1:
-                string = "||regex(?pred_inv&, &&)"
-                filter_query_pred_inv = filter_query_pred_inv + string.replace("&&", str(self.filter_set[i-1]))
-        filter_query_pred_inv = filter_query_pred_inv + ")"
+        filter_query_pred = self.filter_query_pred_gen()
+        filter_query_pred_inv = self.filter_query_pred_inv_gen()
+        filter_query_vertex = self.filter_query_vertex_gen()
+        filter_query_vertex_mid = filter_query_vertex + filter_query_vertex.replace("&", "&&")
+        filter_query_vertex_mid = filter_query_vertex_mid.replace(")FILTER(", "||")
 
-        query_part2 = """
+        query_part2_open = """
     WHERE {
+        """
+
+        query_part2_a = """
+        { {
+            <""" + self.start_page + """> ?pred1 ?n1
+        } UNION {
+            ?n1 ?pred_inv1 <""" + self.start_page + """>
+        } } .
+        """
+        query_part2_b = """
         { {
             """ + filter_query_pred.replace("&", "1") + """
             <""" + self.start_page + """> ?pred1 ?n1
@@ -120,20 +178,46 @@ class PairwiseSchemaBuilder(SchemaBuilder):
             ?n1 ?pred_inv1 <""" + self.start_page + """>
         } } .
         """
-        for i in range(depth-2):
-            i = i + 1
-            block = """
+        query_part2_c = """
         { {
-            """ + filter_query_pred.replace("&", str(i+1)) + """
+            """ + filter_query_vertex.replace("&", "1") + """
+            <""" + self.start_page + """> ?pred1 ?n1
+        } UNION {
+            """ + filter_query_vertex.replace("&", "1") + """
+            ?n1 ?pred_inv1 <""" + self.start_page + """>
+        } } .
+        """
+        for i in range(depth - 2):
+            block_a = """
+        { {
             ?n& ?pred&& ?n&&
         } UNION {
-            """ + filter_query_pred_inv.replace("&", str(i+1)) + """
             ?n&& ?pred_inv&& ?n&
         } } .
             """
-            query_part2 = query_part2 + block.replace("&&", str(i+1)).replace("&", str(i))
+            block_b = """
+        { {
+            """ + filter_query_pred.replace("&", str(i + 2)) + """
+            ?n& ?pred&& ?n&&
+        } UNION {
+            """ + filter_query_pred_inv.replace("&", str(i + 2)) + """
+            ?n&& ?pred_inv&& ?n&
+        } } .
+            """
+            block_c = """
+        { {
+            """ + filter_query_vertex_mid + """
+            ?n& ?pred&& ?n&&
+        } UNION {
+            """ + filter_query_vertex_mid + """
+            ?n&& ?pred_inv&& ?n&
+        } } .
+            """
+            query_part2_a = query_part2_a + block_a.replace("&&", str(i + 2)).replace("&", str(i + 1))
+            query_part2_b = query_part2_b + block_b.replace("&&", str(i + 2)).replace("&", str(i + 1))
+            query_part2_c = query_part2_c + block_c.replace("&&", str(i + 2)).replace("&", str(i + 1))
 
-        final_block = """
+        final_block_a = """
         { {
             """ + filter_query_pred.replace("&", str(depth)) + """
             ?n& ?pred&& <""" + self.end_page + """>
@@ -141,11 +225,44 @@ class PairwiseSchemaBuilder(SchemaBuilder):
             """ + filter_query_pred_inv.replace("&", str(depth)) + """
             <""" + self.end_page + """> ?pred_inv&& ?n&
         } } .
+        """
+        final_block_b = """
+        { {
+            """ + filter_query_pred.replace("&", str(depth)) + """
+            ?n& ?pred&& <""" + self.end_page + """>
+        } UNION {
+            """ + filter_query_pred_inv.replace("&", str(depth)) + """
+            <""" + self.end_page + """> ?pred_inv&& ?n&
+        } } .
+        """
+        final_block_c = """
+        { {
+            """ + filter_query_vertex + """
+            ?n& ?pred&& <""" + self.end_page + """>
+        } UNION {
+            """ + filter_query_vertex + """
+            <""" + self.end_page + """> ?pred_inv&& ?n&
+        } } .
+        """
+        query_part2_a = query_part2_a + final_block_a.replace("&&", str(depth)).replace("&", str(depth-1))
+        query_part2_b = query_part2_b + final_block_b.replace("&&", str(depth)).replace("&", str(depth-1))
+        query_part2_c = query_part2_c + final_block_c.replace("&&", str(depth)).replace("&", str(depth-1))
+
+        query_part2_close = """
     }
         """
-        query_part2 = query_part2 + final_block.replace("&&", str(depth)).replace("&", str(depth-1))
+
+        if len(self.filter_set_edges) == 0 and len(self.filter_set_vertices) == 0:
+            query_part2 = query_part2_open + query_part2_a + query_part2_close
+        elif len(self.filter_set_edges) != 0 and len(self.filter_set_vertices) != 0:
+            query_part2 = query_part2_open + query_part2_b + query_part2_c + query_part2_close
+        elif len(self.filter_set_vertices) == 0:
+            query_part2 = query_part2_open + query_part2_b + query_part2_close
+        elif len(self.filter_set_edges) == 0:
+            query_part2 = query_part2_open + query_part2_c + query_part2_close
 
         query = query_part1 + query_part2
+        print(query)
         return query
 
     def cypher_query_gen(self, depth, url):
@@ -153,9 +270,8 @@ class PairwiseSchemaBuilder(SchemaBuilder):
 
         query_part2 = "MERGE (n0:page:start_page {iri: \"" + self.start_page + "\"})\r"
         for i in range(depth-1):
-            i = i + 1
             string = "MERGE (n&:page {iri: row.n&})\r"
-            query_part2 = query_part2 + string.replace("&", str(i))
+            query_part2 = query_part2 + string.replace("&", str(i + 1))
         final_string = "MERGE (n&:page:end_page {iri: \"" + self.end_page + "\"})\r"
         query_part2 = query_part2 + final_string.replace("&", str(depth))
 
@@ -165,7 +281,7 @@ class PairwiseSchemaBuilder(SchemaBuilder):
     FOREACH (x IN CASE WHEN row.pred&& IS NULL THEN [] ELSE [1] END | MERGE (n&)-[p:pred {iri: row.pred&&}]->(n&&))
     FOREACH (x IN CASE WHEN row.pred_inv&& IS NULL THEN [] ELSE [1] END | MERGE (n&)<-[p:pred {iri: row.pred_inv&&}]-(n&&))
             """
-            query_part3 = query_part3 + block.replace("&&", str(i+1)).replace("&", str(i))
+            query_part3 = query_part3 + block.replace("&&", str(i + 1)).replace("&", str(i))
 
         query_part4 = "\rRETURN "
         for i in range(depth):
@@ -179,51 +295,81 @@ class PairwiseSchemaBuilder(SchemaBuilder):
 
 
 class ParentSchemaBuilder(SchemaBuilder):
-    def __init__(self, page, filter_set):
+    def __init__(self, page, filter_set_edges=[], filter_set_vertices=[]):
         self.name = "ParentSchemaBuilder on " + page
         self.page = page
-        self.filter_set = filter_set
+        self.filter_set_edges = filter_set_edges
+        self.filter_set_vertices = filter_set_vertices
 
     def sparql_query_gen(self, depth):
         query_part1 = "\rSELECT "
         for i in range(depth):
-            i = i + 1
             string = "?pred& ?n& "
-            query_part1 = query_part1 + string.replace("&", str(i))
-        
-        for i in range(len(self.filter_set)):
-            if i == 0:
-                filter_query_pred = "FILTER("
-                string = "regex(?pred&, &&)"
-                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set[i-1]))
-            elif i <= len(self.filter_set) - 1:
-                string = "||regex(?pred&, &&)"
-                filter_query_pred = filter_query_pred + string.replace("&&", str(self.filter_set[i-1]))
-        filter_query_pred = filter_query_pred + ")"
+            query_part1 = query_part1 + string.replace("&", str(i + 1))
 
-        query_part2 = """
+        filter_query_pred = self.filter_query_pred_gen()
+        filter_query_vertex = self.filter_query_vertex_gen()
+        filter_query_vertex_mid = filter_query_vertex + filter_query_vertex.replace("&", "&&")
+        filter_query_vertex_mid = filter_query_vertex_mid.replace(")FILTER(", "||")
+
+        query_part2_open = """
     WHERE {
+        """
+
+        query_part2_a = """
+        {
+            <""" + self.page + """> ?pred1 ?n1
+        } .
+        """
+        query_part2_b = """
         {
             """ + filter_query_pred.replace("&", "1") + """
             <""" + self.page + """> ?pred1 ?n1
         } .
         """
-        for i in range(depth-1):
-            i = i + 1
-            block = """
+        query_part2_c = """
         {
-            """ + filter_query_pred.replace("&", str(i+1)) + """
+            """ + filter_query_vertex.replace("&", "1") + """
+            <""" + self.page + """> ?pred1 ?n1
+        } .
+        """
+        for i in range(depth - 1):
+            block_a = """
+        {
             ?n& ?pred&& ?n&&
         } .
             """
-            query_part2 = query_part2 + block.replace("&&", str(i+1)).replace("&", str(i))
+            block_b = """
+        {
+            """ + filter_query_pred.replace("&", "&&") + """
+            ?n& ?pred&& ?n&&
+        } .
+            """
+            block_c = """
+        {
+            """ + filter_query_vertex_mid + """
+            ?n& ?pred&& ?n&&
+        } .
+            """
+            query_part2_a = query_part2_a + block_a.replace("&&", str(i + 2)).replace("&", str(i + 1))
+            query_part2_b = query_part2_b + block_b.replace("&&", str(i + 2)).replace("&", str(i + 1))
+            query_part2_c = query_part2_c + block_c.replace("&&", str(i + 2)).replace("&", str(i + 1))
 
-        final_string = """
+        query_part2_close = """
     }
         """
-        query_part2 = query_part2 + final_string
+
+        if len(self.filter_set_edges) == 0 and len(self.filter_set_vertices) == 0:
+            query_part2 = query_part2_open + query_part2_a + query_part2_close
+        elif len(self.filter_set_edges) != 0 and len(self.filter_set_vertices) != 0:
+            query_part2 = query_part2_open + query_part2_b + query_part2_c + query_part2_close
+        elif len(self.filter_set_vertices) == 0:
+            query_part2 = query_part2_open + query_part2_b + query_part2_close
+        elif len(self.filter_set_edges) == 0:
+            query_part2 = query_part2_open + query_part2_c + query_part2_close
 
         query = query_part1 + query_part2
+        print(query)
         return query
 
     def cypher_query_gen(self, depth, url):
@@ -231,16 +377,15 @@ class ParentSchemaBuilder(SchemaBuilder):
 
         query_part2 = "MERGE (n0:page {iri: \"" + self.page + "\"})\r"
         for i in range(depth):
-            i = i + 1
             string = "MERGE (n&:page {iri: row.n&})\r"
-            query_part2 = query_part2 + string.replace("&", str(i))
+            query_part2 = query_part2 + string.replace("&", str(i + 1))
 
         query_part3 = ""
         for i in range(depth):
             block = """
     FOREACH (x IN CASE WHEN row.pred&& IS NULL THEN [] ELSE [1] END | MERGE (n&)-[p:pred {iri: row.pred&&}]->(n&&))
             """
-            query_part3 = query_part3 + block.replace("&&", str(i+1)).replace("&", str(i))
+            query_part3 = query_part3 + block.replace("&&", str(i + 1)).replace("&", str(i))
 
         query_part4 = "\rRETURN "
         for i in range(depth):
@@ -252,19 +397,25 @@ class ParentSchemaBuilder(SchemaBuilder):
         query = query_part1 + query_part2 + query_part3 + query_part4
         return query
 
+# WIP
+class PopulateSchemaBuilder(SchemaBuilder):
+    def __init__(self, page):
+        self.name = "PopulateSchemaBuilder on " + page
+        self.page = page
+
 
 commit_cypher_code("MATCH (x) DETACH DELETE (x)")
 
-# sch1 = PairwiseSchemaBuilder("http://dbpedia.org/resource/Television", "http://dbpedia.org/resource/Netflix", ["dct:subject", "skos:broader"])
+# sch1 = PairwiseSchemaBuilder("http://dbpedia.org/resource/Television", "http://dbpedia.org/resource/Netflix", filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
 # sch1.build_at_depth(2)
 # sch1.build_at_depth(3)
 # sch1.build_at_depth(4)
 
-sch2a = ParentSchemaBuilder("http://dbpedia.org/resource/Netflix", ["dct:subject", "skos:broader", "rdf:type"])
-sch2a.build_at_depth(2)
+# sch2a = ParentSchemaBuilder("http://dbpedia.org/resource/Netflix", filter_set_edges=["dct:subject", "skos:broader", "rdf:type"], filter_set_vertices=[])
+# sch2a.build_at_depth(2)
 
-sch2b = ParentSchemaBuilder("http://dbpedia.org/resource/Television", ["dct:subject", "skos:broader", "rdf:type"])
-sch2b.build_at_depth(2)
+# sch2b = ParentSchemaBuilder("http://dbpedia.org/resource/Television", filter_set_edges=["dct:subject", "skos:broader", "rdf:type"], filter_set_vertices=[])
+# sch2b.build_at_depth(2)
 
 # TODO LIST
 # + Clean up the code into one file.
@@ -274,5 +425,9 @@ sch2b.build_at_depth(2)
 # + Test out different search filters and optimise.
 # + Write a class for building parent schema.
 # + Write an inheritance class for PairwiseSchemaBuilder and ParentSchemaBuilder.
+# + Clean up code.
+# + Add options for node/ edge specific filters.
+# + Move some node/ edge filter code to SchemaBuilder class.
+# - Create a PopulateSchemaBuilder subclass that populates all immediate neighbours.
+# - Update the build_at_depth() strings.
 # - Instead of using higher depths, try looking at immediate neighbours with many edges, and use them as the new start/end nodes from which to build new schema.
-# - Add options for node/ edge specific filters.
