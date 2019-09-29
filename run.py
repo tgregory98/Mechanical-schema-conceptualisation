816e69744822cc981f3ef9432752152bfd876543
Tristan <40725906+tgregory98@users.noreply.github.com>
2019-09-29 02:04:43 +0100

    Added endorsed url list for quick access

--- run.py
+++ run.py
@@ -26,26 +26,30 @@ commit_cypher_query("""
     DETACH DELETE (x)
     """)
 
+url1 = "http://dbpedia.org/resource/Netflix"
+url2 = "http://dbpedia.org/resource/Television"
+url3 = "http://dbpedia.org/resource/Category:Complex_systems_theory"
+url4 = "http://dbpedia.org/resource/Category:Systems_theory"
 
-# sch1 = modules.builders.PairwiseSchemaBuilder("http://dbpedia.org/resource/Television", "http://dbpedia.org/resource/Netflix", filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
+# sch1 = modules.builders.PairwiseSchemaBuilder(url1, url2, filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
 # commit_cypher_query_set(sch1.cypher_query_set_gen(2))
 # commit_cypher_query_set(sch1.cypher_query_set_gen(3))
 # commit_cypher_query_set(sch1.cypher_query_set_gen(4))
 
 
-sch2a = modules.builders.ParentSchemaBuilder("http://dbpedia.org/resource/Netflix", filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
+sch2a = modules.builders.ParentSchemaBuilder(url1, filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
 commit_cypher_query_set(sch2a.cypher_query_set_gen(3))
 
-sch2b = modules.builders.ParentSchemaBuilder("http://dbpedia.org/resource/Television", filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
+sch2b = modules.builders.ParentSchemaBuilder(url2, filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
 commit_cypher_query_set(sch2b.cypher_query_set_gen(3))
 
 
-# sch3a = modules.builders.PopulateSchemaBuilder("http://dbpedia.org/resource/Category:Complex_systems_theory", filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
+# sch3a = modules.builders.PopulateSchemaBuilder(url3, filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
 # commit_cypher_query_set(sch3a.cypher_query_set_gen(1))
 
-# sch3b = modules.builders.PopulateSchemaBuilder("http://dbpedia.org/resource/Category:Systems_theory", filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
+# sch3b = modules.builders.PopulateSchemaBuilder(url4, filter_set_edges=["dct:subject", "skos:broader"], filter_set_vertices=[])
 # commit_cypher_query_set(sch3b.cypher_query_set_gen(1))
 
 
-cl1 = modules.cleaners.LeafSchemaCleaner(["http://dbpedia.org/resource/Netflix", "http://dbpedia.org/resource/Television"])
+cl1 = modules.cleaners.LeafSchemaCleaner([url1, url2])
 commit_cypher_query_set(cl1.cypher_query_set_gen(3))
